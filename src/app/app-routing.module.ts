0e5404bc1858d90596ff6a3b1e93776d4538d8ae
Hassan Sani <saniyhassan@gmail.com>
2020-06-10 04:13:47 +0100

    auth created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
