00249402acab8cb21e1000379b24e9138c401d16
Hassan Sani <saniyhassan@gmail.com>
2020-06-26 04:17:58 +0100

    implemented chart

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [HomeComponent, SettingsComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartsModule } from 'ng2-charts';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SummaryComponent } from './components/summary/summary.component';
 
 
 @NgModule({
-  declarations: [SideBarComponent, HeaderComponent],
+  declarations: [SideBarComponent, HeaderComponent, SummaryComponent],
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ChartsModule
   ],
-  exports: [SideBarComponent, HeaderComponent]
+  exports: [SideBarComponent, HeaderComponent, SummaryComponent]
 })
 export class SharedModule { }
