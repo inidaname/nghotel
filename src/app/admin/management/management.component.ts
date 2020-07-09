5bc0dc158b1c3d8bb9d3b2c1ff714136d9634ef2
Hassan Sani <saniyhassan@gmail.com>
2020-07-09 09:24:15 +0100

    implementing management

---
+++ src/app/admin/management/management.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './management.component.html',
+  styleUrls: ['./management.component.scss']
+})
+export class ManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
