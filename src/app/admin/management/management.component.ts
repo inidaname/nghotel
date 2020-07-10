b5708d6219faf55421c85e32c2a38ee4afc49fff
Hassan Sani <saniyhassan@gmail.com>
2020-07-10 08:03:50 +0100

    worked on tabs switch

--- src/app/admin/management/management.component.ts
+++ src/app/admin/management/management.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './management.component.html',
@@ -12,12 +13,16 @@ export class ManagementComponent implements OnInit {
   manages: any; //this will be from database
   manages$: Observable<any>; //check this
   sub: any;
+  pageTitle: string;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private title: Title
   ) {
     this.manages$ = this.route.params;
+    this.pageTitle = ' Management';
+    title.setTitle(this.title.getTitle()+this.pageTitle)
   }
 
   ngOnInit(): void {
