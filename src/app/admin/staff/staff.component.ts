007013afaf7a99fffb8c98534ed0175eb36ed1ce
Hassan Sani <saniyhassan@gmail.com>
2020-09-12 13:10:58 +0100

    working on staff management

---
+++ src/app/admin/staff/staff.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  templateUrl: './staff.component.html',
+  styleUrls: ['./staff.component.scss']
+})
+export class StaffComponent implements OnInit {
+  manages: any; //this will be from database
+  manages$: Observable<any>; //check this
+  sub: any;
+  pageTitle: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private title: Title
+  ) {
+    this.pageTitle = ' Staff Management';
+    this.manages$ = this.route.params;
+    title.setTitle(this.title.getTitle()+this.pageTitle)
+  }
+
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.manages = params['manage'];
+    });
+  }
+
+}
