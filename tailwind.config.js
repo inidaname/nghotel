78d21410645734b2d69c774cbaec5b79699dabab
Hassan Sani <saniyhassan@gmail.com>
2020-11-16 00:19:23 +0100

    working on roomservice

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
         'width': 'width',
         'height': 'height'
       }
+    },
+    inset: {
+      '0': '0',
+      '20': '20px',
+      '30': '30px',
+      '50': '50px',
+      '1/2': '50%',
+      '2/2': '20%'
+    },
+    height: {
+      '500': '500px',
+      '600': '650px',
+      '480': '480px'
     }
   },
   variants: {},
