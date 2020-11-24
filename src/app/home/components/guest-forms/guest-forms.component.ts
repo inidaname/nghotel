7439690fa257b2bd998563384f191e8f2bba0cf4
Hassan Sani <saniyhassan@gmail.com>
2020-11-24 15:39:20 +0100

    added dynamic forms

---
+++ src/app/home/components/guest-forms/guest-forms.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-forms',
+  templateUrl: './guest-forms.component.html',
+  styleUrls: ['./guest-forms.component.scss']
+})
+export class GuestFormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
---
+++ src/app/shared/interface/guestform.ts
@@ -0,0 +1,35 @@
+export interface Guestform {
+}
+
+export class GuestformTB<T> {
+  value: T;
+  key: string;
+  label: string;
+  required: boolean;
+  order: number;
+  controlType: string;
+  type: string;
+  options: {key: string; value: string}[];
+
+  constructor(options: Options<T> = {}){
+    this.value = options.value;
+    this.key = options.key;
+    this.label = options.label || '';
+    this.required = !!options.required;
+    this.order = options.order === undefined ? 1 : options.order;
+    this.controlType = options.controlType || '';
+    this.type = options.type || '';
+    this.options = options.options || [];
+  }
+}
+
+export interface Options<T> {
+  value?: T;
+  key?: string;
+  label?: string;
+  required?: boolean;
+  order?: number;
+  controlType?: string;
+  type?: string;
+  options?: {key: string; value: string}[];
+}
