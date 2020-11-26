49cc52e74cbf3e0b82ef26fa5133e7048a620cb4
Hassan Sani <saniyhassan@gmail.com>
2020-11-26 22:06:10 +0100

    set title for page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nghotel';
+  title = 'All Rooms | ';
+
+  constructor(
+    private mainPage: Title,
+    ) {
+    mainPage.setTitle(this.title);
+  }
+
 }
